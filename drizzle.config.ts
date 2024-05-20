64650073664494b19bda15910c430e150d855e5f
Ogie Galicia <o.galicia.cpa@gmail.com>
2024-05-20 21:43:06 +0800

    chore: update dependencies

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,17 +1,15 @@
-import type { Config } from 'drizzle-kit';
-import 'dotenv/config';
+import { defineConfig } from 'drizzle-kit';
 
 const url =
 	process.env.TURSO_CONFIG === 'dev' ? process.env.TURSO_LOCAL : process.env.TURSO_CONNECTION_URL;
 
-const config = {
+export default defineConfig({
 	schema: './src/lib/server/db/schema',
 	out: './migrations',
 	driver: 'turso',
+	dialect: 'sqlite',
 	dbCredentials: {
-		url: url,
+		url: url!,
 		authToken: process.env.TURSO_AUTH_TOKEN
 	}
-};
-
-export default config satisfies Config;
+});
